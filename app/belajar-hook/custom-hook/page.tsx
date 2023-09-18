a2f2e1af0af78b8fd54288932909b771204b6f5f
Kingostyl <emr8770@gmail.com>
2023-09-18 11:20:46 +0700

    d

--- app/belajar-hook/custom-hook/page.tsx
+++ app/belajar-hook/custom-hook/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 import { useRef, useState } from "react";
 import Button from "../../component/button";
-
+import { useClosure } from "@/hook";
 
 const Home = () => {
-  let [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const onOpen = () => {
-    setIsOpen(true);
-  };
-
-  const onClose = () => {
-    setIsOpen(false);
-  };
+  const { isOpen, onOpen, onClose } = useClosure();
 
   return (
     <section className="h-screen w-screen space-y-5">
-     <Button onClick={onOpen} colorSchema="blue" variant="solid" title="open" />
-     <Button onClick={onClose} colorSchema="red" variant="solid" title="closed" />
+      <Button
+        onClick={onOpen}
+        colorSchema="blue"
+        variant="solid"
+        title="open"
+      />
+      <Button
+        onClick={onClose}
+        colorSchema="red"
+        variant="solid"
+        title="closed"
+      />
 
-
-     {isOpen ? <p>Open</p> : <p>Close</p>}
-
+      {isOpen ? <p>Open</p> : <p>Close</p>}
     </section>
   );
 };
